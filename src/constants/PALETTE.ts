caba9df70f1c5ccc4c09af8c72c4f49da071b425
codes <codesver@gmail.com>
2025-12-18 21:40:16 +0900

    feat: compose palette

---
+++ src/constants/PALETTE.ts
@@ -0,0 +1,25 @@
+export const MAIN = {
+  primary: "#127CEA",
+  secondary: "#558CC7",
+  tertiary: "#061D38",
+} as const;
+
+export const GRAYSCALE = {
+  white: "#FFFFFF",
+  gray50: "#F0F4F8",
+  gray100: "#E2E6EA",
+  gray200: "#C9CDD1",
+  gray300: "#B0B4B8",
+  gray400: "#979B9F",
+  gray500: "#7E8286",
+  gray600: "#65696D",
+  gray700: "#4C5054",
+  gray800: "#33373B",
+  gray900: "#1A1E22",
+  black: "#000000",
+} as const;
+
+export const SEMANTIC = {
+  error: "#E91051",
+  warning: "#FF9500",
+} as const;
