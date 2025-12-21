eb0b02dc5a6d81480e1cabb5f28253bc4948a7cc
codes <codesver@gmail.com>
2025-12-21 22:52:30 +0900

    feat(button): publish button base style

--- src/themes/button-theme.ts
+++ src/themes/button-theme.ts
@@ -2,7 +2,20 @@ import { createTheme } from "@mui/material";
 
 const buttonTheme = createTheme({
   components: {
-    MuiButton: {},
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          width: "fit-content",
+          height: "32px",
+          padding: "0px 12px",
+          boxShadow: "none",
+        },
+        sizeMedium: {
+          height: "32px",
+          padding: "0px 12px",
+        },
+      },
+    },
   },
 });
 
