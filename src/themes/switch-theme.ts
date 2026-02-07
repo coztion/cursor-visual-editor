8521bbb9c076c1e599a3cde8014ef3670150eb63
codes <codesver@gmail.com>
2026-02-07 18:20:17 +0900

    feat(switch): publish background

--- src/themes/switch-theme.ts
+++ src/themes/switch-theme.ts
@@ -2,7 +2,26 @@ import { createTheme } from "@mui/material";
 
 const switchTheme = createTheme({
   components: {
-    MuiSwitch: {},
+    MuiSwitch: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          padding: 0,
+          borderStyle: "solid",
+          borderWidth: "1px",
+          borderColor: theme.palette.grayscale.gray700,
+          borderImage: "none",
+          borderRadius: "50px",
+        }),
+        track: ({ theme }) => ({
+          backgroundColor: theme.palette.grayscale.white,
+        }),
+        switchBase: ({ theme }) => ({
+          "&.Mui-checked + .MuiSwitch-track": {
+            backgroundColor: theme.palette.main.primary,
+          },
+        }),
+      },
+    },
   },
 });
 
